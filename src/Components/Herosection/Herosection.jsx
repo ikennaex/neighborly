77e41449ea473f802b3c5c17bdcbbc7b6fa9f0c6
Excel Ikenna <ikennaexcel@gmail.com>
2025-02-15 17:06:15 +0100

    new elements

--- src/Components/Herosection/Herosection.jsx
+++ src/Components/Herosection/Herosection.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { fridge } from '../../imports'
 
 const Herosection = () => {
   return (
     <div className=''>
-      <div className='flex items-center bg-customYellow h-72 rounded-xl'>
+      <div className='flex items-center bg-customBlue h-72 rounded-xl'>
         <div className='py-3 px-4 w-1/2'>
-            <p className='text-2xl font-bold mb-7'>
+        <div className='mb-7 text-white'>
+            <p className='text-2xl font-bold mb-2'>
         Best Deals in your Neighbourhood
             </p>
+            <p className='italic'>...buy with ease</p>
+        </div>
 
-            <button className='bg-white p-3 border-black font-semibold '>Shop now</button>
+            <button className='bg-customYellow p-3 border-black font-semibold '>Shop now</button>
+        </div>
+
+        <div>
+            <img className='h-72 object-cover' src= {fridge} alt="" />
         </div>
       </div>
     </div>
---
+++ src/imports.js
@@ -0,0 +1,5 @@
+import fridge from "./assets/fridge.png"
+
+export {
+    fridge,
+}
