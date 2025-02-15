2e4ededc593e48739f61cd85ac6e2302bce46792
Excel Ikenna <ikennaexcel@gmail.com>
2025-02-15 17:14:42 +0100

    updates

--- src/Components/Herosection/Herosection.jsx
+++ src/Components/Herosection/Herosection.jsx
@@ -4,7 +4,7 @@ import { fridge } from '../../imports'
 const Herosection = () => {
   return (
     <div className=''>
-      <div className='flex items-center bg-customBlue h-72 rounded-xl'>
+      <div className='flex items-center bg-customBlue h-72 rounded-xl overflow-auto'>
         <div className='py-3 px-4 w-1/2'>
         <div className='mb-7 text-white'>
             <p className='text-2xl font-bold mb-2'>
@@ -16,8 +16,8 @@ const Herosection = () => {
             <button className='bg-customYellow p-3 border-black font-semibold '>Shop now</button>
         </div>
 
-        <div>
-            <img className='h-72 object-cover' src= {fridge} alt="" />
+        <div className='mt-52'>
+            <img className='h-80 object-cover' src= {fridge} alt="" />
         </div>
       </div>
     </div>
