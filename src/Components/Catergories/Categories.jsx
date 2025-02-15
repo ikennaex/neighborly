7dffdd64012ac353e967fdcc914a477d510bd257
Excel Ikenna <ikennaexcel@gmail.com>
2025-02-15 18:11:47 +0100

    updates

--- src/Components/Catergories/Categories.jsx
+++ src/Components/Catergories/Categories.jsx
@@ -32,7 +32,7 @@ const Categories = () => {
         <div className='flex flex-wrap gap-3 justify-center'>
             {items.map((item, index) => (
                 
-                <div className='w-1/4 h-24 bg-stone-300 p-3 flex flex-col justify-between items-center ' key={index}>
+                <div className='w-1/4 h-24 bg-stone-300 rounded-xl p-3 flex flex-col justify-between items-center ' key={index}>
                     <img className='w-16 h-10' src={item.img} alt={item.text} />
                     <p className='text-[0.7rem] leading-[0.8rem]' href={item.link}>{item.text}</p>
                 </div>
