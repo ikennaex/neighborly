3c71c03cb15b2400683b7a647d11e12443357af1
Excel Ikenna <ikennaexcel@gmail.com>
2025-02-16 10:03:52 +0100

    search bar

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className=''>
+      <form action="">
+        <input className='h-10 p-2 mb-4 rounded-md w-full bg-stone-300' type="text" placeholder='Search for products in your area'/>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Herosection from '../../Components/Herosection/Herosection'
 import Categories from '../../Components/Catergories/Categories'
 import Products from '../../Components/Products/Products'
+import SearchBar from '../../Components/SearchBar/SearchBar'
 
 const Homepage = () => {
   return (
     <div className='container mx-auto'>
       <div className='lg:p-7 p-3'>
+        <SearchBar />
         <Herosection />
         <Categories />
         <Products />
