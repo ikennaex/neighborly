44f24a217c4ce277d6d418ec69e5362f7766c9f7
Fortune <fortuneokechukwu206@gmail.com>
2025-02-24 10:16:25 +0100

    update vendor 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Homepage from './Pages/Homepage/Homepage'
 import Footer from './Components/Footer/Footer'
 import Allproducts from './Pages/AllProducts/Allproducts'
 import ProductPage from './Pages/ProductPage/ProductPage'
-import VendorPage from './Pages/Vendorpage/Vendorpage'
+import VendorProducts from './Pages/Vendorpage/VendorProducts'
 
 function App() {
 
@@ -20,7 +20,7 @@ function App() {
         <Route path = "/register" element = {<Register />} />
         <Route path = "/allproducts" element = {<Allproducts />} />
         <Route path = "/products/:id" element = {<ProductPage />} />
-        <Route path = "/vendor/:id" element = {<Vendorpage />} />
+        <Route path = "/vendor/:id" element = {<VendorProducts />} />
       </Routes>
       <Footer />
 
