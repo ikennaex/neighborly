7e725bcb0bc32de65f00024d01cecbcb3455b55d
Excel Ikenna <ikennaexcel@gmail.com>
2025-03-09 12:41:20 +0100

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,90 +1,101 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from "react";
 // import {logo} from "../import"
-import { Link } from 'react-router'
+import { Link } from "react-router";
 import { IoIosMenu } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
-import { UserContext } from '../../UserContext';
+import { UserContext } from "../../UserContext";
 
 const Navbar = () => {
-    const menuItems = [
-      {
-        name: "Categories",
-        link: "/categories"
-      },
-      {
-        name: "About Us",
-        link: "/about-us"
-      },
+  const menuItems = [
+    {
+      name: "Categories",
+      link: "/categories",
+    },
+    {
+      name: "About Us",
+      link: "/about-us",
+    },
 
-      {
-        name: "Become vendor",
-        link: "/becomeavendor"
-      },
-    ]
+    {
+      name: "Become vendor",
+      link: "/becomeavendor",
+    },
+  ];
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const toggle = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
   return (
-    <div className='container mx-auto px-4 py-4 bg-white border-b-2'>
-        <div className='lg:py-0.5 flex items-center justify-between'>
-        <Link to = "/" >
-        <img className="logo w-28 lg:w-36 h-auto" src="/images/awoofbuyerlogo.png" alt="logo"></img>
+    <div className="container mx-auto px-4 py-4 bg-white border-b-2">
+      <div className="lg:py-0.5 flex items-center justify-between">
+        <Link to="/">
+          <img
+            className="logo w-28 lg:w-36 h-auto"
+            src="/images/awoofbuyerlogo.png"
+            alt="logo"
+          ></img>
         </Link>
 
-        <div className='hidden gap-20 mx-auto lg:flex cursor-pointer'>
-            {menuItems.map(menu => (
-              <Link to={menu.link} className='nav-link'>
-                {/* <p>Categories</p> */}
-                <p className='nav-menu hover:text-customBlue'>{menu.name}</p>
-                {/* {menu.name} */}
+        <div className="hidden gap-20 mx-auto lg:flex cursor-pointer">
+          {menuItems.map((menu) => (
+            <Link to={menu.link} className="nav-link">
+              {/* <p>Categories</p> */}
+              <p className="nav-menu hover:text-customBlue">{menu.name}</p>
+              {/* {menu.name} */}
+            </Link>
+
+            // <a href= {menu.link}>
+            // </a>
+          ))}
+
+          {user && (
+            <div className="bg-customBlue text-white rounded-lg px-5 py-2">
+              <Link to="/user:id">
+                <p>Hi {user.username}</p>
               </Link>
-
-              // <a href= {menu.link}>
-              // </a>
-            ))}
-
-            {user && (
-              <div className='bg-customBlue text-white rounded-lg px-5 py-2'>
-                <Link to = "/user:id">
-                
-                <p>Hello {user.username}</p>
-                </Link>
-              </div>
-            )}
-
+            </div>
+          )}
         </div>
 
         {/* toggle menu   */}
-        <div className='lg:hidden' onClick={toggle}>
-          {open? <IoClose  size={40} className='menu-icon' /> : <IoIosMenu size={40} className='menu-icon' /> }
+        <div className="lg:hidden" onClick={toggle}>
+          {open ? (
+            <IoClose size={40} className="menu-icon" />
+          ) : (
+            <IoIosMenu size={40} className="menu-icon" />
+          )}
         </div>
-        </div>
+      </div>
 
-        {/* Mobile menu open */}
-        <div>
-            {open && 
-          <div className='flex flex-col gap-5 py-5'>
+      {/* Mobile menu open */}
+      <div>
+        {open && (
+          <div className="flex flex-col gap-5 py-5">
             {menuItems.map((menu) => {
               return (
-              <Link to={menu.link} className='nav-link border-b-2'>
-                {menu.name}
-                {/* {menu.name} */}
-              </Link>
-              )
-              
+                <Link to={menu.link} className="nav-link border-b-2">
+                  {menu.name}
+                  {/* {menu.name} */}
+                </Link>
+              );
             })}
 
+            {user && (
+              <div className="bg-customBlue text-white rounded-lg px-5 py-2">
+                <Link to="/user:id">
+                  <p>Hello {user.username}</p>
+                </Link>
+              </div>
+            )}
           </div>
-          }
-        </div>
+        )}
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
