e8cc5ed5803598a2162e9d9a3cdfbca070d481f2
Excel Ikenna <ikennaexcel@gmail.com>
2025-03-09 19:24:49 +0100

    endpoint update

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,1 +1,5 @@
-export const baseUrl = "http://localhost:4000/" || "https://awoofbuyer-api.onrender.com"
+const isDev = false; // set to true for local development
+
+export const baseUrl = isDev
+  ? "http://localhost:4000/"
+  : "https://awoofbuyer-api.onrender.com";
