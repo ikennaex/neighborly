f7f26477dbb10d768fbc0393fd8f35afb2bea6d8
Excel Ikenna <ikennaexcel@gmail.com>
2025-03-09 19:31:32 +0100

    new endpoint

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,4 +2,4 @@ const isDev = false; // set to true for local development
 
 export const baseUrl = isDev
   ? "http://localhost:4000/"
-  : "https://awoofbuyer-api.onrender.com";
+  : "https://awoofbuyer-api.onrender.com/";
