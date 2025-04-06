d514e15622d29f4b0a601cd45204d70d2dd877b8
Excel Ikenna <ikennaexcel@gmail.com>
2025-04-07 00:02:38 +0100

    new changes

--- src/Components/Catergories/Categories.jsx
+++ src/Components/Catergories/Categories.jsx
@@ -29,11 +29,11 @@ const Categories = () => {
       <div className='py-7'>
         <p className='text-xl font-bold text-center mb-5 '>what are you looking for ?</p>
 
-        <div className='flex gap-3 overflow-x-scroll w-full whitespace-nowrap'>
+        <div className='flex gap-3 md:overflow-x-scroll w-full whitespace-nowrap justify-center'>
             {items.map((item, index) => (
                 <div key={index} className='flex-shrink-0'>
 
-                <div className='lg:w-full w-24 lg:h-full h-28 bg-stone-200 rounded-xl py-7  flex flex-col justify-between items-center'>
+                <div className='lg:w-52 w-24 lg:h-full h-28 bg-stone-200 rounded-xl py-7  flex flex-col justify-between items-center'>
                     <img className='lg:h-14 h-12 pb-2' src={item.img} alt={item.text} />
                     <p className='text-[0.7rem] leading-[0.8rem] text-center' href={item.link}>{item.text}</p>
                 </div>
