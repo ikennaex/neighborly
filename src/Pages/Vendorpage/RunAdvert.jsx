09045d3e165aa85c60777acba072c04bc926d967
ikennaex <ikennaexcel@gmail.com>
2025-08-08 08:36:39 +0100

    fix payment downtime

--- src/Pages/Vendorpage/RunAdvert.jsx
+++ src/Pages/Vendorpage/RunAdvert.jsx
@@ -1,37 +1,37 @@
-import axios from 'axios';
-import React, { useContext, useState } from 'react'
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import Loader from "../../Loader/Loader"; // Import Loader component
-import { baseUrl } from '../../baseUrl';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
-import PaystackPop from '@paystack/inline-js'
+import { baseUrl } from "../../baseUrl";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import PaystackPop from "@paystack/inline-js";
 
-const weeklyAd = import.meta.env.VITE_WEEKLY_AD
-const monthlyAd = import.meta.env.VITE_MONTHLY_AD
+const weeklyAd = import.meta.env.VITE_WEEKLY_AD;
+const monthlyAd = import.meta.env.VITE_MONTHLY_AD;
 
 const RunAdvert = () => {
-    const [preview, setPreview] = useState(null)
-    const [loading, setLoading] = useState(false)
+  const [preview, setPreview] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const { user } = useContext(UserContext);
-    const userName = user.firstName + " " + user.lastName;
-    const vendorId = user._id;
-    const vendorName = user.businessName;
-    const vendorEmail = user.email;
-    const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+  const userName = user.firstName + " " + user.lastName;
+  const vendorId = user._id;
+  const vendorName = user.businessName;
+  const vendorEmail = user.email;
+  const navigate = useNavigate();
 
-      const [product, setProduct] = useState({
-        duration: "",
-        name: "",
-        vendorName: "",
-        desc: "",
-        link: "",
-        price: "",
-        location: "",
-        img: null,
-      });
+  const [product, setProduct] = useState({
+    duration: "",
+    name: "",
+    vendorName: "",
+    desc: "",
+    link: "",
+    price: "",
+    location: "",
+    img: null,
+  });
 
-      // Handle Input Changes
+  // Handle Input Changes
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
@@ -45,145 +45,205 @@ const RunAdvert = () => {
     }
   };
 
-    const submitAd = async (reference) => {
-      setLoading(true);
-    
-      const formData = new FormData();
-      formData.append("name", product.name);
-      formData.append("vendorName", vendorName);
-      formData.append("desc", product.desc);
-      formData.append("link", product.link);
-      formData.append("price", product.price.toString().trim());
-      formData.append("img", product.img);
-      formData.append("duration", product.duration);
-      formData.append("location", product.location);
-      formData.append("reference", reference);
-    
-      try {
-        const response = await axios.post(`${baseUrl}runadvert`, formData, {
-          headers: { "Content-Type": "multipart/form-data" },
-        });
-    
-        setProduct({
-          name: "",
-          desc: "",
-          link: "",
-          price: "",
-          img: null,
-          duration: "",
-          location: "",
-        });
-        setPreview(null);
-        navigate(`/user/${user._id}`);
-      } catch (err) {
-        console.log(err);
-        alert("Failed to upload product");
-        return false;
-      } finally {
-        setLoading(false);
-      }
-    };
+  const submitAd = async (reference) => {
+    setLoading(true);
 
-      // handle payment 
-    // Paystack Payment
+    const formData = new FormData();
+    formData.append("name", product.name);
+    formData.append("vendorName", vendorName);
+    formData.append("desc", product.desc);
+    formData.append("link", product.link);
+    formData.append("price", product.price.toString().trim());
+    formData.append("img", product.img);
+    formData.append("duration", product.duration);
+    formData.append("location", product.location);
+    formData.append("reference", reference);
 
-    const handlePayment = () => {
-      const paystack = new PaystackPop()
-        paystack.newTransaction ({
-            key: "pk_live_9baae4fd8eb89d362e310e5978a0918e40f66b62",
-            amount: product.duration === "weekly" ? weeklyAd*100 : monthlyAd*100,
-            email: user.email,
-            firstname: user.firstName  || "John",
-            lastname: user.lastName || "Doe",
-            onSuccess(transaction) {
-              submitAd(transaction.reference)
-    
-          },
-          onCancel() {
-            setLoading(false)
-              alert("Payment Cancelled")
-          }
-        })
+    try {
+      const response = await axios.post(`${baseUrl}runadvert`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+      setProduct({
+        name: "",
+        desc: "",
+        link: "",
+        price: "",
+        img: null,
+        duration: "",
+        location: "",
+      });
+      setPreview(null);
+      navigate(`/user/${user._id}`);
+    } catch (err) {
+      console.log(err);
+      alert("Failed to upload product");
+      return false;
+    } finally {
+      setLoading(false);
     }
-    
-    // Form submission handler
-    const handleSubmit = (e) => {
-      e.preventDefault();
+  };
 
-      handlePayment()
-    
-    };
+  // handle payment
+  // Paystack Payment
 
+  const handlePayment = () => {
+    const paystack = new PaystackPop();
+    paystack.newTransaction({
+      key: "pk_live_9baae4fd8eb89d362e310e5978a0918e40f66b62",
+      amount: product.duration === "weekly" ? weeklyAd * 100 : monthlyAd * 100,
+      email: user.email,
+      firstname: user.firstName || "John",
+      lastname: user.lastName || "Doe",
+      onSuccess(transaction) {
+        setTimeout(() => {
+          submitAd(transaction.reference);
+        }, 1000); // delay to ensure transaction is processed
+      },
+      onCancel() {
+        setLoading(false);
+        alert("Payment Cancelled");
+      },
+    });
+  };
 
+  // Form submission handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    handlePayment();
+  };
 
   return (
-    <div className='container'>
-        {loading ? <Loader /> :
-      <div className='mx-auto p-4'>
-        <div>
-        <h2 className='font-bold text-3xl text-center'>Sponsored Ads</h2>
-        <p className='text-center'>Get your product on the spotlight</p>
-        </div>
+    <div className="container">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="mx-auto p-4">
+          <div>
+            <h2 className="font-bold text-3xl text-center">Sponsored Ads</h2>
+            <p className="text-center">Get your product on the spotlight</p>
+          </div>
 
-        <form onSubmit={handleSubmit} action="">
+          <form onSubmit={handleSubmit} action="">
             <div>
-                {/* set duration  */}
-                <div className='mb-4'>
-            <label className='block text-sm font-medium"'>Set Duration</label>
-            <select value={product.duration} onChange={handleChange} name = "duration" className='w-full border rounded-lg p-2 mt-1 text-sm'>
-            <option value="">Select Duration</option>
-            <option value="weekly">Weekly</option>
-            <option value="monthly">Monthly</option>
+              {/* set duration  */}
+              <div className="mb-4">
+                <label className='block text-sm font-medium"'>
+                  Set Duration
+                </label>
+                <select
+                  value={product.duration}
+                  onChange={handleChange}
+                  name="duration"
+                  className="w-full border rounded-lg p-2 mt-1 text-sm"
+                >
+                  <option value="">Select Duration</option>
+                  <option value="weekly">Weekly</option>
+                  <option value="monthly">Monthly</option>
+                </select>
+              </div>
 
-            </select>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">
+                  Product Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={product.name}
+                  onChange={handleChange}
+                  required
+                  className="w-full border rounded-lg p-2 mt-1"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-sm font-medium">
+                  Product Description
+                </label>
+                <textarea
+                  name="desc"
+                  value={product.desc}
+                  onChange={handleChange}
+                  required
+                  className="w-full border rounded-lg p-2 mt-1"
+                ></textarea>
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">
+                  Product Price ($)
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  value={product.price}
+                  onChange={handleChange}
+                  required
+                  className="w-full border rounded-lg p-2 mt-1"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-sm font-medium">
+                  Product Link
+                </label>
+                <input
+                  placeholder="Link to your product"
+                  type="text"
+                  name="link"
+                  value={product.link}
+                  onChange={handleChange}
+                  required
+                  className="w-full border rounded-lg p-2 mt-1"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Location</label>
+                <input
+                  name="location"
+                  value={product.location}
+                  onChange={handleChange}
+                  required
+                  className="w-full border rounded-lg p-2 mt-1"
+                  placeholder="eg: Lekki, Lagos"
+                />
+              </div>
+              <p className="font-bold text-customBlue">
+                NOTE:FOR BEST RESULT USE IMAGES WITHOUT BACKGROUND
+              </p>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">
+                  Product Image
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="w-full border rounded-lg p-2 mt-1"
+                />
+                {preview && (
+                  <img
+                    src={preview}
+                    alt="Preview"
+                    className="mt-3 w-32 h-32 object-cover rounded-lg"
+                  />
+                )}
+              </div>
             </div>
 
-            <div className='mb-4'>
-            <label className="block text-sm font-medium">Product Name</label>
-            <input type="text" name="name" value= {product.name} onChange={handleChange} required className="w-full border rounded-lg p-2 mt-1" />
-          </div>
-
-          <div className='mb-4'>
-            <label className="block text-sm font-medium">Product Description</label>
-            <textarea name="desc" value={product.desc} onChange={handleChange} required className="w-full border rounded-lg p-2 mt-1"></textarea>
-          </div>
-          <div className='mb-4'>
-            <label className="block text-sm font-medium">Product Price ($)</label>
-            <input type="number" name="price" value={product.price} onChange={handleChange} required className="w-full border rounded-lg p-2 mt-1" />
-          </div>
-
-          <div className='mb-4'>
-            <label className="block text-sm font-medium">Product Link</label>
-            <input placeholder="Link to your product" type="text" name="link" value={product.link} onChange={handleChange} required className="w-full border rounded-lg p-2 mt-1" />
-          </div>
-
-          <div className='mb-4'>
-            <label className="block text-sm font-medium">Location</label>
-            <input name="location" value={product.location} onChange= {handleChange} required className="w-full border rounded-lg p-2 mt-1" placeholder="eg: Lekki, Lagos" />
-          </div>
-          <p className='font-bold text-customBlue'>NOTE:FOR BEST RESULT USE IMAGES WITHOUT BACKGROUND</p>
-            <div className='mb-4'>
-            <label className="block text-sm font-medium">Product Image</label>
-            <input type="file" accept="image/*" onChange={handleImageChange} className="w-full border rounded-lg p-2 mt-1" />
-            {preview && <img src={preview} alt="Preview" className="mt-3 w-32 h-32 object-cover rounded-lg" />}
-          </div>
-            </div>
-
-        <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
-          Pay with Paystack
-        </button>
-
-        </form>
-      </div>
-    }
+            <button className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+              Pay with Paystack
+            </button>
+          </form>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default RunAdvert
-
-
+export default RunAdvert;
 
 // const config = {
 //   public_key: "FLWPUBK_TEST-75fc0559e51d8343f858be2da3ae0403-X",
@@ -202,7 +262,6 @@ export default RunAdvert
 //     logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
 //   },
 // };
-
 
 // const fwConfig = {
 //   ...config,
@@ -237,4 +296,3 @@ export default RunAdvert
 //     console.log("Payment modal closed without completing transaction.");
 //   },
 // };
-
